d4fb5d3eab4f80dc6216f4053a0102685ddc0b47
haksoat <shopejuh@gmail.com>
2019-04-30 22:47:15 +0100

    Function to parse downloaded page

--- page_analyser.py
+++ page_analyser.py
@@ -1,10 +1,17 @@
 import requests
+from bs4 import BeautifulSoup as bs
 
 class PageAnalyser():
 	def __init__(self, url):
 		self.url = url
 		self.page_content = None
+		self.parsed_page_content = None
 
 	def download_page(self):
 		page_object = requests.get(self.url)
 		self.page_content = page_object.content
+
+	def parse_page(self):
+		self.parsed_page_content = bs(self.page_content, "html.parser")
+
+		
