c9b79d6668bf084b58b32488ad9585d803f68c47
haksoat <shopejuh@gmail.com>
2019-04-30 23:26:14 +0100

    Refactoring

--- page_analyser.py
+++ page_analyser.py
@@ -6,24 +6,42 @@ class PageAnalyser():
 		self.url = url
 		self.page_content = None
 		self.parsed_page_content = None
-		self.anchor_tags = []
+		self.anchor_tags = None
 		self.href_values = []
 		self.anchor_text = []
 
 	def download_page(self):
-		page_object = requests.get(self.url)
+		try:
+			if "https://" not in self.url:
+				page_object = requests.get("https://{}".format(self.url))
+			else:
+				page_object = requests.get(self.url)
+		except requests.exceptions.ConnectionError:
+			print("Error: Something is wrong with the url")
 		self.page_content = page_object.content
 
 	def parse_page(self):
-		self.parsed_page_content = bs(self.page_content, "html.parser")
+		if self.page_content is None:
+			print("Error: Page content has not been loaded")
+		else:
+			self.parsed_page_content = bs(self.page_content, "html.parser")
 
-	def get_anchor_tags(self):
-		self.anchor_tags = self.parsed_page_content.find_all("a")
+	def find_anchor_tags(self):
+		if self.parsed_page_content is None:
+			print("Error: Page content has not been parsed")
+		else:
+			self.anchor_tags = self.parsed_page_content.find_all("a")
 
-	def get_href_values(self):
-		for anchor_tag in self.anchor_tags:
-			self.href_values.append(anchor_tag["href"])
+	def find_href_values(self):
+		if self.anchor_tags is None:
+			print("Error: There are no anchor tags")
+		else:
+			for anchor_tag in self.anchor_tags:
+				self.href_values.append(anchor_tag["href"])
 
-	def get_anchor_text(self):
-		for anchor_tag in self.anchor_tags:
-			self.anchor_text.append(anchor_tag.text.strip())
+	def find_anchor_texts(self):
+		if self.anchor_tags is None:
+			print("Error: There are no anchor tags")
+		else:
+			for anchor_tag in self.anchor_tags:
+				self.anchor_text.append(anchor_tag.text.strip())
