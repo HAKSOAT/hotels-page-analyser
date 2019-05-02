a8827825277db7ae57b680b6e105f163a37efa5c
haksoat <shopejuh@gmail.com>
2019-05-02 22:44:52 +0100

    Refactoring

--- page_analyser.py
+++ page_analyser.py
@@ -10,20 +10,22 @@ from urllib.parse import urlsplit
 
 class PageAnalyser():
 	def __init__(self, url):
-		self.url = url
+		# requests.get only works with urls with http or https
+		# so the url needs to have http or https in it
+		if re.match(r"(https://|http://).+", url):
+			self.url = url
+		else:
+			self.url = "http://{}".format(url)
 
-	def get_page_content(self):
+	def get_page_content(self, url=None):
+		if url is None:
+			url = self.url 
 		try:
 			# Some sites do not allow requests without headers
 			# The headers dictionary deals with this
 			headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) \
-			AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
-			# requests.get only works with urls with http or https
-			if re.match(r"(https://|http://).+", self.url):
-				page_object = requests.get(self.url, headers=headers)
-			else:
-				self.url = "http://{}".format(self.url)
-				page_object = requests.get(self.url, headers=headers)
+			AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}			
+			page_object = requests.get(url, headers=headers)
 			page_content = page_object.content
 		except requests.exceptions.ConnectionError:
 			print("Error: Something is wrong with the url")
@@ -63,27 +65,27 @@ class PageAnalyser():
 			link_domain_name = urlparse(link).netloc
 			if (link_domain_name == domain_name):
 				link_one_level_deep = self.get_link_one_level_down(link)
+				print(link_one_level_deep)
 				link_text_mapping.setdefault(link_one_level_deep,[]).append(anchor_text)
-			else:
-				pass
 		return link_text_mapping
 
-	def get_meta_data(self, links_text_mapping):
+	def get_meta_data(self, link_text_mapping):
+		for link, anchor_text in link_text_mapping.items():
+			# requests.get only works with urls with http or https
+			# so the url needs to have http or https in it
+			if not re.match(r"(https://|http://).+", link):
+				link = "http://{}".format(link)
+			article = Article(link)
+			article.download()
+			content = article.html
+			article.parse()
+			article.nlp()
+			keywords = article.keywords
+			summary = article.summary
+			data = self.get_page_content(link)
+			h1=[i.get_text() for i  in data.find_all("h1")]
+			return(link,anchor_text, h1, summary, keywords)
 
-
-		for key,value in links_text_mapping.items():
-   		 	url=urlparse(key)
-   			article = Article(key)
-   		 	article.download()
-   		 	content = article.html
-    	 	article.parse()
-  		 	article.nlp()
-    		keywords = article.keywords
-	    	summary = article.summary
-
-    	 	data= bs(url, 'html.parser')
-    		h1=[i.get_text() for i  in data.find_all("h1")]
-          	return(key,value, h1, summary, keywords)
 def main():
 	parser = argparse.ArgumentParser()
 	parser.add_argument("-l", "--link", help='add a url to test', type=str)
@@ -92,7 +94,7 @@ def main():
 	page_content = pageanalyser.get_page_content()
 	links_and_anchor_texts = pageanalyser.get_all_links_and_anchor_texts(page_content)
 	internal_links_and_anchor_texts = pageanalyser.get_internal_links_and_anchor_texts(links_and_anchor_texts)
-
+	meta_data = pageanalyser.get_meta_data(internal_links_and_anchor_texts)
 	
 if __name__ == '__main__':
 	main()
