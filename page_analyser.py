04ce5c417598f2d05361f743aa9a20f15ad55bd5
haksoat <shopejuh@gmail.com>
2019-05-02 12:32:28 +0100

    Rewrote algorithm

--- page_analyser.py
+++ page_analyser.py
@@ -9,16 +9,8 @@ import requests
 class PageAnalyser():
 	def __init__(self, url):
 		self.url = url
-		self.page_content = None
-		self.parsed_page_content = None
-		self.anchor_tags = None
-		self.href_values = []
-		self.anchor_texts = []
-		self.h_tags = None
-		self.h_tag_texts = []
-		self.BoW = []
 
-	def download_page(self):
+	def get_page_content(self):
 		try:
 			# Some sites do not allow requests without headers
 			# The headers dictionary deals with this
@@ -29,97 +21,37 @@ class PageAnalyser():
 				page_object = requests.get(self.url, headers=headers)
 			else:
 				page_object = requests.get("https://{}".format(self.url), headers=headers)
-			self.page_content = page_object.content
+			page_content = page_object.content
 		except requests.exceptions.ConnectionError:
 			print("Error: Something is wrong with the url")
+		parsed_page_content = bs(page_content, "html.parser")
+		return parsed_page_content
 
-	def parse_page(self):
-		if self.page_content is None:
-			print("Error: Page content has not been loaded")
-		else:
-			self.parsed_page_content = bs(self.page_content, "html.parser")
+	def get_links_and_anchor_texts(self, page_content):
+		anchor_tags = page_content.find_all("a")
+		links = []
+		anchor_text = []
+		for anchor_tag in anchor_tags:
+			# Some anchor tags do not have href attributes
+			# Use an empty string as the href value of such tags
+			try:
+				links.append(anchor_tag["href"])
+			except KeyError:
+				links.append("")
+			anchor_text.append(anchor_tag.text.strip())
+		links_and_anchor_texts = list(zip(links, anchor_text))
+		return links_and_anchor_texts
 
-	def find_anchor_tags(self):
-		if self.parsed_page_content is None:
-			print("Error: Page content has not been parsed")
-		else:
-			self.anchor_tags = self.parsed_page_content.find_all("a")
 
-	def find_href_values(self):
-		if self.anchor_tags is None:
-			print("Error: There are no anchor tags")
-		else:
-			for anchor_tag in self.anchor_tags:
-				# Some anchor tags do not have href attributes
-				# Use an empty string as the href value of such tags
-				try:
-					self.href_values.append(anchor_tag["href"])
-				except KeyError:
-					self.href_values.append("")
-
-	def find_anchor_texts(self):
-		if self.anchor_tags is None:
-			print("Error: There are no anchor tags")
-		else:
-			for anchor_tag in self.anchor_tags:
-				self.anchor_texts.append(anchor_tag.text.strip())
-
-	def get_links_and_texts(self):
-		links_and_texts = list(zip(self.href_values, self.anchor_texts))
-		return links_and_texts
-
-	def find_h_tags(self):
-		if self.parsed_page_content is None:
-			print("Error: Page content has not been parsed")
-		else:
-			self.h_tags = self.parsed_page_content.find_all("h1")
-			self.h_tags += self.parsed_page_content.find_all("h2")
-			self.h_tags += self.parsed_page_content.find_all("h3")
-
-	def find_h_tag_texts(self):
-		if self.h_tags is None:
-			print("Error: There are no h tags")
-		else:
-			for h_tag in self.h_tags:
-				self.h_tag_texts.append(h_tag.text.strip())
-
-	def clean_text(self, text):
-		ignored_words = ['a', "the", "is"]
-		words = re.sub("[^\w]", " ",  text).split()
-		cleaned_text = [word.lower() for word in words if word not in ignored_words]
-		return cleaned_text
-
-	def tokenize(self, texts):
-		words = []
-		for text in texts:
-		    cleaned_text = self.clean_text(text)
-		    words.extend(cleaned_text)   
-		words = sorted(list(set(words)))
-		return words
-
-	def get_BoW(self):
-		texts = self.h_tag_texts
-		vocab = self.tokenize(self.h_tag_texts)
-		for text in texts:
-			cleaned_texts = self.clean_text(text)
-			bag_vector = numpy.zeros(len(vocab))
-			for cleaned_text in cleaned_texts:
-				for index, word in enumerate(vocab):
-					if word == cleaned_text: 
-						bag_vector[index] += 1
-			self.BoW.append((text, numpy.array(bag_vector)))
-		return self.BoW
   
 def main():
 	parser = argparse.ArgumentParser()
 	parser.add_argument("-l", "--link", help='add a url to test', type=str)
 	args = parser.parse_args()
 	pageanalyser = PageAnalyser(args.link)
-	pageanalyser.download_page()
-	pageanalyser.parse_page()
-	pageanalyser.find_anchor_tags()
-	pageanalyser.find_href_values()
-	pageanalyser.find_anchor_texts()
+	page_content = pageanalyser.get_page_content()
+	links_and_anchor_texts = pageanalyser.get_links_and_anchor_texts(page_content)
+	print(links_and_anchor_texts)
 	
 if __name__ == '__main__':
 	main()
