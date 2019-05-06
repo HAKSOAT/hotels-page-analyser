0855db3a2d5368ff33de38a70a0258092137856d
haksoat <shopejuh@gmail.com>
2019-05-06 10:33:09 +0100

    Added method to create a corpus from data structure, labels and urls for the created corpus

--- page_classifier.py
+++ page_classifier.py
@@ -1,3 +1,22 @@
 class PageClassifier():
 	def __init__(self, data_structure):
 		self.data_structure = data_structure
+
+	def get_corpus_labels_urls(self, keyword):
+		labels = []
+		urls = []
+		corpus = []
+		for mapping in self.data_structure:
+			for key, site in mapping.items():
+				for key, page in site.items():
+				page = page[0]
+				corpus.append("{} {} {}".format(page["short_url"], page["keywords"],
+												page["anchor_tags"], page["h1"]))
+				if keyword in page["short_url"]:
+				       labels.append(1)
+				else:
+				       labels.append(0)
+				urls.append(page["url"])
+
+		return (corpus, labels, urls)
+
