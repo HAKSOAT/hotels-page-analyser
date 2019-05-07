2eecf8067df0f427741cc03b3c9d865cd90eabb2
Jesse <amamgbujesse@yahoo.com>
2019-05-07 18:49:06 +0100

    Contact page model

--- app.py
+++ app.py
@@ -5,107 +5,119 @@ from page_analyser import PageAnalyser
 from page_classifier import PageClassifier
 
 def main():  #this calls the class and the methods. Coded by @Haks
-	parser = argparse.ArgumentParser()
-	parser.add_argument("-t", "--train", help="Use to train models", type=str)
-	parser.add_argument("-p", "--predict", help="Use to predict pages", type=str)
-	parser.add_argument("-n", "--number", help="Use to choose the number of sites to train with", type=int)
-	args = parser.parse_args()
-	if (args.train and not args.number):
-		parser.error("To train the model, you need to pass in the number of links to be used")
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-t", "--train", help="Use to train models", type=str)
+    parser.add_argument("-p", "--predict", help="Use to predict pages", type=str)
+    parser.add_argument("-n", "--number", help="Use to choose the number of sites to train with", type=int)
+    args = parser.parse_args()
+    if (args.train and not args.number):
+        parser.error("To train the model, you need to pass in the number of links to be used")
 
-	# Runs the code here when user wants to train the models
-	if args.train:
-		# Create a directory for the dataset to keep the csv files
-		current_directory = os.path.dirname(os.path.abspath(__file__))
-		dataset_directory = os.path.join(current_directory, "dataset")
-		file_path = os.path.join(dataset_directory, args.train)
-		try:
-			with open(file_path) as f:
-				rows = csv.reader(f)
-				links = [row for row in rows]
-		except FileNotFoundError:
-			parser.error("File does not exist!!!")
-		# Get a data structure from the links stated in the dataset
-		number_of_links = args.number
-		for link in links[:number_of_links]:
-			pageanalyser = PageAnalyser(link[0])
-			page_content = pageanalyser.get_page_content()
-			# Deals with links that do not return anything
-			if page_content is None:
-				continue
-			try:
-				links_and_anchor_texts = pageanalyser.get_all_links_and_anchor_texts(page_content)
-				internal_links_and_anchor_texts = pageanalyser.get_internal_links_and_anchor_texts(links_and_anchor_texts)
-				print("Getting meta data, this may take a while.")
-				meta_data = pageanalyser.get_meta_data(internal_links_and_anchor_texts)
-				pageanalyser.get_data_structure(meta_data)
-			except:
-				continue
+    # Runs the code here when user wants to train the models
+    if args.train:
+        # Create a directory for the dataset to keep the csv files
+        current_directory = os.path.dirname(os.path.abspath(__file__))
+        dataset_directory = os.path.join(current_directory, "dataset")
+        file_path = os.path.join(dataset_directory, args.train)
+        try:
+            with open(file_path) as f:
+                rows = csv.reader(f)
+                links = [row for row in rows]
+        except FileNotFoundError:
+            parser.error("File does not exist!!!")
+        # Get a data structure from the links stated in the dataset
+        number_of_links = args.number
+        for link in links[:number_of_links]:
+            pageanalyser = PageAnalyser(link[0])
+            page_content = pageanalyser.get_page_content()
+            # Deals with links that do not return anything
+            if page_content is None:
+                continue
+            try:
+                links_and_anchor_texts = pageanalyser.get_all_links_and_anchor_texts(page_content)
+                internal_links_and_anchor_texts = pageanalyser.get_internal_links_and_anchor_texts(links_and_anchor_texts)
+                print("Getting meta data, this may take a while.")
+                meta_data = pageanalyser.get_meta_data(internal_links_and_anchor_texts)
+                pageanalyser.get_data_structure(meta_data)
+            except:
+                continue
 
-		# Extract the data structure, create a corpus and train the models
-		data_structure = PageAnalyser.data_structure
-		pageclassifier = PageClassifier(data_structure)
-		privacy_corpus, privacy_labels, _ = pageclassifier.get_corpus_labels_urls("privacy")
-		about_corpus, about_labels, _ = pageclassifier.get_corpus_labels_urls("about")
-		rooms_corpus, rooms_labels, _ = pageclassifier.get_corpus_labels_urls("rooms")
-		pageclassifier.train_privacy_pages(privacy_corpus, privacy_labels)
-		pageclassifier.train_about_pages(about_corpus, about_labels)
-		pageclassifier.train_rooms_pages(rooms_corpus,rooms_labels)
-		print("Training Complete")
+        # Extract the data structure, create a corpus and train the models
+        data_structure = PageAnalyser.data_structure
+        pageclassifier = PageClassifier(data_structure)
+        privacy_corpus, privacy_labels, _ = pageclassifier.get_corpus_labels_urls("privacy")
+        about_corpus, about_labels, _ = pageclassifier.get_corpus_labels_urls("about")
+        rooms_corpus, rooms_labels, _ = pageclassifier.get_corpus_labels_urls("rooms")
+        contact_corpus, contact_labels, _ = pageclassifier.get_corpus_labels_urls("contact") 
+        pageclassifier.train_privacy_pages(privacy_corpus, privacy_labels)
+        pageclassifier.train_about_pages(about_corpus, about_labels)
+        pageclassifier.train_rooms_pages(rooms_corpus,rooms_labels)
+        pageclassifier.train_rooms_pages(contact_corpus, contact_labels) 
+        print("Training Complete")
 
-	# Runs the code here when user wants to predict pages based on the trained models
-	elif args.predict:
-		link = args.predict
-		pageanalyser = PageAnalyser(link)
-		page_content = pageanalyser.get_page_content()
+    # Runs the code here when user wants to predict pages based on the trained models
+    elif args.predict:
+        link = args.predict
+        pageanalyser = PageAnalyser(link)
+        page_content = pageanalyser.get_page_content()
 
-		# Script should quit if nothing exists in the url inputted
-		if page_content is None:
-			quit()
-		
-		# Extract the data structure and create a corpus
-		links_and_anchor_texts = pageanalyser.get_all_links_and_anchor_texts(page_content)
-		internal_links_and_anchor_texts = pageanalyser.get_internal_links_and_anchor_texts(links_and_anchor_texts)
-		print("Getting metadata, this may take a while.")
-		meta_data = pageanalyser.get_meta_data(internal_links_and_anchor_texts)
-		pageanalyser.get_data_structure(meta_data)
-		data_structure = PageAnalyser.data_structure
-		pageclassifier = PageClassifier(data_structure)
-		corpus, _, urls = pageclassifier.get_corpus_labels_urls("")
+        # Script should quit if nothing exists in the url inputted
+        if page_content is None:
+            quit()
+        
+        # Extract the data structure and create a corpus
+        links_and_anchor_texts = pageanalyser.get_all_links_and_anchor_texts(page_content)
+        internal_links_and_anchor_texts = pageanalyser.get_internal_links_and_anchor_texts(links_and_anchor_texts)
+        print("Getting metadata, this may take a while.")
+        meta_data = pageanalyser.get_meta_data(internal_links_and_anchor_texts)
+        pageanalyser.get_data_structure(meta_data)
+        data_structure = PageAnalyser.data_structure
+        pageclassifier = PageClassifier(data_structure)
+        corpus, _, urls = pageclassifier.get_corpus_labels_urls("")
 
-		# JavaScript intensive sites will come up with lots of errors
-		# Such as the unavailabilty of links in the returned content from requests
-		# This triggers an error when predicting, so try and except deals with that
-		try:
-			privacy_predictions = pageclassifier.predict_privacy_pages(corpus)
-			about_predictions = pageclassifier.predict_about_pages(corpus)
-			rooms_predictions = pageclassifier.predict_rooms_pages(corpus)
+        # JavaScript intensive sites will come up with lots of errors
+        # Such as the unavailabilty of links in the returned content from requests
+        # This triggers an error when predicting, so try and except deals with that
+        try:
+            privacy_predictions = pageclassifier.predict_privacy_pages(corpus)
+            about_predictions = pageclassifier.predict_about_pages(corpus)
+            rooms_predictions = pageclassifier.predict_rooms_pages(corpus)
+            contact_predictions = pageclassifier.predict_contact_pages(corpus)
 
-			if 1 in about_predictions:
-				for prediction, url in zip(about_predictions, urls):
-					if prediction == 1:
-						print("{} ===>>> About Page".format(url))
-			else:
-				print("No About Page")
-			
-			if 1 in privacy_predictions:
-				for prediction, url in zip(privacy_predictions, urls):
-					if prediction == 1:
-						print("{} ===>>> Privacy Page".format(url))
-			else:
-				print("No Privacy Page")
+            if 1 in about_predictions:
+                for prediction, url in zip(about_predictions, urls):
+                    if prediction == 1:
+                        print("{} ===>>> About Page".format(url))
+            else:
+                print("No About Page")
+            
+            if 1 in privacy_predictions:
+                for prediction, url in zip(privacy_predictions, urls):
+                    if prediction == 1:
+                        print("{} ===>>> Privacy Page".format(url))
+            else:
+                print("No Privacy Page")
 
-			if 1 in rooms_predictions:
-				for prediction, url in zip(rooms_predictions, urls):
-					if prediction == 1:
-						print("{} ===>>> Rooms Page".format(url))
-			else:
-				print("No Rooms Page")
+            if 1 in rooms_predictions:
+                for prediction, url in zip(rooms_predictions, urls):
+                    if prediction == 1:
+                        print("{} ===>>> Rooms Page".format(url))
+            else:
+                print("No Rooms Page")
+                
+                
+            if 1 in contact_predictions:
+                for prediction, url in zip(contact_predictions, urls):
+                    if prediction == 1:
+                        print("{} ===>>> Contact Page".format(url))
+            else:
+                print("No Contact Page")
 
-		except ValueError:
-			print("Sadly, the url doesn't like to be scraped")
 
-		
-	
+        except ValueError:
+            print("Sadly, the url doesn't like to be scraped")
+
+        
+    
 if __name__ == '__main__':
-	main()
+    main()
--- page_classifier.py
+++ page_classifier.py
@@ -8,86 +8,102 @@ from sklearn.pipeline import Pipeline
 
 
 class PageClassifier():
-	def __init__(self, data_structure):
-		self.data_structure = data_structure
+    def __init__(self, data_structure):
+        self.data_structure = data_structure
 
-	def get_corpus_labels_urls(self, keyword):
-		labels = []
-		urls = []
-		corpus = []
-		for mapping in self.data_structure:
-			for key, site in mapping.items():
-				for key, page in site.items():
-					page = page[0]
-					corpus.append("{} {} {}".format(page["short_url"], page["keywords"],
-													page["anchor_tags"], page["h1"]))
-					if keyword in page["short_url"]:
-					    labels.append(1)
-					else:
-					    labels.append(0)
-					urls.append(page["url"])
+    def get_corpus_labels_urls(self, keyword):
+        labels = []
+        urls = []
+        corpus = []
+        for mapping in self.data_structure:
+            for key, site in mapping.items():
+                for key, page in site.items():
+                    page = page[0]
+                    corpus.append("{} {} {}".format(page["short_url"], page["keywords"],
+                                                    page["anchor_tags"], page["h1"]))
+                    if keyword in page["short_url"]:
+                        labels.append(1)
+                    else:
+                        labels.append(0)
+                    urls.append(page["url"])
 
-		return (corpus, labels, urls)
+        return (corpus, labels, urls)
 
-	def save_model(self, classifier, file_name):
-		current_directory = os.path.dirname(os.path.abspath(__file__))
-		models_directory = os.path.join(current_directory, "models")
+    def save_model(self, classifier, file_name):
+        current_directory = os.path.dirname(os.path.abspath(__file__))
+        models_directory = os.path.join(current_directory, "models")
 
-		if not os.path.exists(models_directory):
-			os.makedirs(models_directory)
+        if not os.path.exists(models_directory):
+            os.makedirs(models_directory)
 
-		file_path = os.path.join(models_directory, file_name)
-		joblib.dump(classifier, file_path)
+        file_path = os.path.join(models_directory, file_name)
+        joblib.dump(classifier, file_path)
 
-	def load_model(self, file_name):
-		current_directory = os.path.dirname(os.path.abspath(__file__))
-		models_directory = os.path.join(current_directory, "models")
-		file_path = os.path.join(models_directory, file_name)
-		classifier = joblib.load(file_path)
-		return classifier
+    def load_model(self, file_name):
+        current_directory = os.path.dirname(os.path.abspath(__file__))
+        models_directory = os.path.join(current_directory, "models")
+        file_path = os.path.join(models_directory, file_name)
+        classifier = joblib.load(file_path)
+        return classifier
 
-	def train_privacy_pages(self, corpus, labels):
-		try:
-			vectorizer = CountVectorizer()
-			SVM = svm.SVC(C=1.0, kernel='linear', degree=3, gamma='auto')
-			classifier = Pipeline([('vectorizer', vectorizer), ('svm', SVM)])
-			classifier.fit(corpus, labels)
-			self.save_model(classifier, "privacy_pages_model.pkl")
-		except ValueError:
-			print("There is no privacy page in the dataset >>> Privacy model, not trained")
+    def train_privacy_pages(self, corpus, labels):
+        try:
+            vectorizer = CountVectorizer()
+            SVM = svm.SVC(C=1.0, kernel='linear', degree=3, gamma='auto')
+            classifier = Pipeline([('vectorizer', vectorizer), ('svm', SVM)])
+            classifier.fit(corpus, labels)
+            self.save_model(classifier, "privacy_pages_model.pkl")
+        except ValueError:
+            print("There is no privacy page in the dataset >>> Privacy model, not trained")
 
-	def predict_privacy_pages(self, corpus):
-		classifier = self.load_model("privacy_pages_model.pkl")
-		predictions = classifier.predict(corpus)
-		return predictions
+    def predict_privacy_pages(self, corpus):
+        classifier = self.load_model("privacy_pages_model.pkl")
+        predictions = classifier.predict(corpus)
+        return predictions
 
-	def train_about_pages(self, corpus, labels):
-		try:
-			vectorizer= CountVectorizer()		
-			multinomial_nb = MultinomialNB(alpha=0.1)
-			classifier = Pipeline([('CountVectorizer', vectorizer), ('naivebayes', multinomial_nb)])
-			classifier.fit(corpus, labels)
-			self.save_model(classifier, "about_pages_model.pkl")
-		except ValueError:
-			print("There is no about page in the dataset >>> About model, not trained")
+    def train_about_pages(self, corpus, labels):
+        try:
+            vectorizer= CountVectorizer()        
+            multinomial_nb = MultinomialNB(alpha=0.1)
+            classifier = Pipeline([('CountVectorizer', vectorizer), ('naivebayes', multinomial_nb)])
+            classifier.fit(corpus, labels)
+            self.save_model(classifier, "about_pages_model.pkl")
+        except ValueError:
+            print("There is no about page in the dataset >>> About model, not trained")
 
-	def predict_about_pages(self, corpus):
-		classifier = self.load_model("about_pages_model.pkl")
-		predictions = classifier.predict(corpus)
-		return predictions
+    def predict_about_pages(self, corpus):
+        classifier = self.load_model("about_pages_model.pkl")
+        predictions = classifier.predict(corpus)
+        return predictions
 
-	def train_rooms_pages(self, corpus, labels):
-		try:
-			vectorizer= CountVectorizer()		
-			logreg = LogisticRegression()
-			classifier = Pipeline([('CountVectorizer', vectorizer), ('Logistic Regression', logreg)])
-			classifier.fit(corpus, labels)
-			self.save_model(classifier, "rooms_pages_model.pkl")
-		except ValueError:
-			print("There is no rooms page in the dataset >>> Rooms model, not trained")
+    def train_rooms_pages(self, corpus, labels):
+        try:
+            vectorizer= CountVectorizer()        
+            logreg = LogisticRegression()
+            classifier = Pipeline([('CountVectorizer', vectorizer), ('Logistic Regression', logreg)])
+            classifier.fit(corpus, labels)
+            self.save_model(classifier, "rooms_pages_model.pkl")
+        except ValueError:
+            print("There is no rooms page in the dataset >>> Rooms model, not trained")
 
-	def predict_rooms_pages(self, corpus):
-		classifier = self.load_model("rooms_pages_model.pkl")
-		predictions = classifier.predict(corpus)
-		return predictions
+    def predict_rooms_pages(self, corpus):
+        classifier = self.load_model("rooms_pages_model.pkl")
+        predictions = classifier.predict(corpus)
+        return predictions
+    
+    
+    def train_contact_pages(self, corpus, labels):
+        try:
+            vectorizer = CountVectorizer()
+            clf = MultinomialNB()
+            classifier = Pipeline([('vectorizer', vectorizer), ('clf', clf)])
+            classifier.fit(corpus, labels)
+            self.save_model(classifier, "contact_pages_model.pkl")
+        except ValueError:
+            print("There is no about page in the dataset >>> Contact model, not trained")
+        
+    def predict_contact_pages(self, corpus):
+        classifier = self.load_model("contact_pages_model.pkl")
+        predictions = classifier.predict(corpus)
+        return predictions
 
