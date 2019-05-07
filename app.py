0d03d0eac8c0e5cb10ffe95ee4c089e49eee1155
haksoat <shopejuh@gmail.com>
2019-05-07 10:32:02 +0100

    Feature to let user pass in number of links to train with

--- app.py
+++ app.py
@@ -8,7 +8,10 @@ def main():  #this calls the class and the methods. Coded by @Haks
 	parser = argparse.ArgumentParser()
 	parser.add_argument("-t", "--train", help="Use to train models", type=str)
 	parser.add_argument("-p", "--predict", help="Use to predict pages", type=str)
+	parser.add_argument("-n", "--number", help="Use to choose the number of sites to train with", type=int)
 	args = parser.parse_args()
+	if (args.train and not args.number):
+		parser.error("To train the model, you need to pass in the number of links to be used")
 
 	# Runs the code here when user wants to train the models
 	if args.train:
@@ -16,11 +19,15 @@ def main():  #this calls the class and the methods. Coded by @Haks
 		current_directory = os.path.dirname(os.path.abspath(__file__))
 		dataset_directory = os.path.join(current_directory, "dataset")
 		file_path = os.path.join(dataset_directory, args.train)
-		with open(file_path) as f:
-			rows = csv.reader(f)
-			links = [row for row in rows]
+		try:
+			with open(file_path) as f:
+				rows = csv.reader(f)
+				links = [row for row in rows]
+		except FileNotFoundError:
+			parser.error("File does not exist!!!")
 		# Get a data structure from the links stated in the dataset
-		for link in links[:50]:
+		number_of_links = args.number + 1
+		for link in links[:number_of_links]:
 			pageanalyser = PageAnalyser(link[0])
 			page_content = pageanalyser.get_page_content()
 			# Deals with links that do not return anything
